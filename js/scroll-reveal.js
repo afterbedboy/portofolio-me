d6d1f7b741e8bdc5f323c78aa7b0fea979627f7f
afterbedboy <152883203+afterbedboy@users.noreply.github.com>
2024-12-14 18:51:02 +0700

    Add files via upload

---
+++ js/scroll-reveal.js
@@ -0,0 +1,64 @@
+// ================== HERO ================== //
+ScrollReveal().reveal('.menu-container',{
+    // delay: 600,
+    origin: 'top',
+    distance: '40px',
+    reset: 'true',
+    duration: '1200'
+});
+
+ScrollReveal().reveal('.hero-left',{
+    delay: 1200,
+    origin: 'left',
+    distance: '450px',
+    duration: '1200'
+});
+
+ScrollReveal().reveal('.hero-right',{
+    delay: 1200,
+    origin: 'right',
+    distance: '450px',
+    duration: '1200'
+});
+
+ScrollReveal().reveal('.wave-blue',{
+    // delay: 3000,
+    origin: 'bottom',
+    distance: '40px',
+    duration: '400'
+});
+
+ScrollReveal().reveal('.wave-coral',{
+    delay: 400,
+    origin: 'bottom',
+    distance: '40px',
+    duration: '400'
+});
+
+// ================== SKILL ================== //
+ScrollReveal().reveal('.left-container',{
+    delay: 400,
+    origin: 'left',
+    distance: '450px',
+    duration: '1200'
+})
+ScrollReveal().reveal('.right-container',{
+    delay: 400,
+    origin: 'right',
+    distance: '450px',
+    duration: '1200'
+})
+ScrollReveal().reveal('.overlay')
+
+// ================== WORK ================== //
+ScrollReveal().reveal('.pb-1',{
+    delay: 1200,
+    origin: 'bottom',
+    distance: '200px',
+    reset: 'true'
+})
+ScrollReveal().reveal('.pb-2',{
+    delay: 600,
+    reset: 'true'
+})
+
